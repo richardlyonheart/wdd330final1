52e8f8f8413686d2b451eeed17c938e48acced0c
richardlyonheart <brendenlyon907@gmail.com>
2024-12-17 19:26:27 -0500

    added files in correct format

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const helloworld = document.createElement('h1');
 helloworld.textContent = "hello world, weather stuff";
 document.getElementById('root').appendChild(helloworld);
+
+import App from './App';
+import { initMap, incrementCounter, displayCounter, getWeatherByLocation, getWeather, getWeatherByLatLon} from "./utils";
+
+document.getElementById('root').appendChild(App());
+
